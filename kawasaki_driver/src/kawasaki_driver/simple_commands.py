4358d8e2a74ca1714faa9509ce3089293785a21a
ntonci <tonci.novkovic@gmail.com>
2017-08-17 13:32:48 +0200

    Added parsers for getting current robot pose and for sending pose

--- kawasaki_driver/src/kawasaki_driver/simple_commands.py
+++ kawasaki_driver/src/kawasaki_driver/simple_commands.py
@@ -1,6 +1,13 @@
 import socket
 import socketserver
 import time
+import math
+
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+import tf
+import rospy
 
 connection_socket
 
@@ -18,23 +25,50 @@ def connect_to_robot(address="192.168.2.2", port=23):
     # might be some delay in-between commands
 
 
-def get_state():
-    global connection_socket
-    connection_socket.send(b'wh\n')
-    time.sleep(0.1)
-    message = connection_socket.recvmsg(500)
-    print(message)
-    # TODO(ntonci): Parse the message
+def state_message_parser(message):
+    split_msg = message.split('\r\n')
+    joint_values = parse_and_convert_to_float(split_msg[2])
+    pose_values = parse_and_convert_to_float(split_msg[4])
+
+    joint_state_message = JointState()
+    joint_state_message.header = Header()
+    joint_state_message.header.stamp = rospy.Time.now()
+    joint_state_message.name = split_msg[1].split()
+    joint_state_message.position = joint_values
+    joint_state_message.velocity = []
+    joint_state_message.effort = []
+
+    pose_state_message = JointState()
+    pose_state_message.header = Header()
+    pose_state_message.header.stamp = rospy.Time.now()
+    pose_state_message.name = split_msg[3].split()
+    pose_state_message.position = pose_values
+    pose_state_message.velocity = []
+    pose_state_message.effort = []
+
+    return joint_state_message, pose_state_message
 
 
-def set_state():
-    global connection_socket
-    # TODO(ntonci): Check how can we send the pose in world frame and start
-    # motion of the robot to go to that pose
-    connection_socket.send('')
-    # TODO(ntonci): We need to wait here for the feedback, it should be
-    # on-blocking I guess
-    message = connection_socket.recvmsg(500)
-    print(message)
-    # TODO(ntonci): Check the output and parse it to check if the execution was
-    # done properly
+def create_pose_message(pose):
+    quaternion = (pose.pose.orientation.x, pose.pose.orientation.y,
+                  pose.pose.orientation.z, pose.pose.orientation.w)
+    euler = tf.transformations.euler_from_quaternion(
+        quaternion)  # TODO(ntonci): Check order of angles and units
+    euler = [math.degrees(angle) for angle in euler]
+    translation = (pose.pose.position.x, pose.pose.position.y,
+                   pose.pose.position.z)
+    message = 'LMOVE '
+    for pos in translation:
+        message = message + str(pos) + ', '
+    for rot in euler:
+        message = message + str(rot) + ', '
+    message = message[:-2] + '\n'
+    return message
+
+
+def parse_and_convert_to_float(string_message):
+    split_string = string_message.split()
+    float_value = []
+    for value in split_string:
+        float_value.append(float(value))
+    return float_value
